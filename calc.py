2a1a1bdf88a2557bccfccc8e82e90cb1e7487093
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-15 11:38:35 +0530

    Auto commit

---
+++ calc.py
@@ -0,0 +1,28 @@
+operator = input("Enter an Operator (+ or - or * or /)")
+
+num1=int(input("Enter the First Number:"))
+num2=int(input("Enter the second Number:"))
+
+if operator == "+":
+    result=num1+num2
+
+elif operator == "-":
+    result=num1-num2
+
+elif operator == "*":
+    result=num1*num2
+
+elif operator ==  "/":
+    if num2!=0:
+        result=num1/num2
+    elif num1>num2:
+        result=float(num1/num2)
+    else:
+        result="Undefined"
+        
+
+else:
+    result="Invalid operator"
+
+
+print(f"{num1} + {operator} + {num2} = {result}")
