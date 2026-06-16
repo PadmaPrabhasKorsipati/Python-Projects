6c804abdf2caf8b284383a3ca99082769ae80cc7
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-16 13:19:03 +0530

    Auto commit

--- timer.py
+++ timer.py
@@ -6,8 +6,8 @@ for x in range(my_time,0,-1):
 
     seconds=x%60
     minutes=int(x/60) %60
-    hours=int(x/3600)%24
-    print(f"00:{minutes:02}:{seconds:02}")
+    hours=int(minutes/60)%24
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
 
     time.sleep(1)
 
