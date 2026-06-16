d1cee6a8ef7493aea07fe3cd12ff178274825a8a
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-16 13:12:44 +0530

    Auto commit

--- timer.py
+++ timer.py
@@ -5,8 +5,8 @@ my_time=int(input("Enter the time in seconds:"))
 for x in range(my_time,0,-1):
 
     seconds=x%60
-    
-    print(f"00:00:{seconds:03}")
+    minutes=x/60
+    print(f"00:{minutes:02}:{seconds:02}")
 
     time.sleep(1)
 
