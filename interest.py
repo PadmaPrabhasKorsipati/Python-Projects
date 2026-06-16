30619cae957edb477093a9cf40c81397c5a7b435
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-16 12:27:24 +0530

    Auto commit

--- interest.py
+++ interest.py
@@ -1,14 +1,37 @@
 
-priniciple=int(input("Enter th principle amount:"))
+principle=0
 
-time=int(input("Enter the duration of loan:"))
+time=0
 
-rate=int(input("Enter the rate of interest:"))
+rate=0
+
+"""time=int(input("Enter the duration of loan:"))
+
+rate=int(input("Enter the rate of interest:"))"""
 
 
-while priniciple<=0:
+while principle<=0:
 
     priniciple=int(input("Enter th principle amount:"))
-    print("Principle amount could not be zero.")
+    if principle <=0:
+      print("Principle amount could not be zero.")
 
    
+while rate<=0:
+   
+   rate=int(input("Entyer the rate of interest:"))
+   if rate<=0:
+      print("Rate could not be zero.")
+
+
+while time<=0:
+   time=int(input("Enter the duration of loan:"))
+
+   if time<=0:
+      print("Time could not be zero.")
+   
+
+
+Total=principle*pow(1+(rate/12),12*time)
+
+print(f"The Total Compound interest is:{Total}")
