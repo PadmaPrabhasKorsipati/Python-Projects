fc4106de661701ddc5d86b0176e0f958de976c4d
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-16 19:54:38 +0530

    Auto commit

--- concession.py
+++ concession.py
@@ -19,3 +19,28 @@ for key,value in menu.items():
     print(f"{key:10}:₹{value:.2f}")
 
 print("------------------------")
+
+
+while True:
+    food=input("Select an item (q to quit):").lower()
+    
+    if food=='q':
+        break
+
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+
+print("---------YOUR CART------")  
+      
+for food in cart:
+
+    total+=menu.get(food)
+
+    print(food,end=" ")
+
+    
+print()
+
+print(f"The total Price of the items in the cart is:₹{total}")
+
